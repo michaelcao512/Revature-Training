4c2ffc724863141e92d5d217a0889dccd50af9ba
Michael <michaelcao512@gmail.com>
2024-12-11 15:20:28 -0500

    added styling to authors list fetch

--- 03/authors.js
+++ 03/authors.js
@@ -13,17 +13,20 @@ function getAuthorsData(){
                 data.forEach( (author) => {
                     // creaet new list element
                     const authorLi = document.createElement('li')
-                      
+                    authorLi.className = 'author'
                     // adds author name to the list element
                     authorLi.innerHTML = author.name
                     
                     // display email and address as a new unordered list inside the author list element    
                     const ulAuthorInformation = document.createElement('ul')
+                    ulAuthorInformation.className = 'author-information'
 
                     const liEmail = document.createElement('li')
+                    liEmail.className = 'email'
                     liEmail.innerHTML = author.email
 
                     const liAddress = document.createElement('li')
+                    liAddress.className = 'address'
                     liAddress.innerHTML = author.address.street + ', ' + author.address.city + ', ' + author.address.zipcode
                    
                     ulAuthorInformation.appendChild(liEmail)
