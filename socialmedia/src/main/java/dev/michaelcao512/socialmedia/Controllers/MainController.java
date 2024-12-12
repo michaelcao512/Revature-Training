1e92b01748a529247555edf1919a2e7fb14a989d
Michael Cao <mcao124@bu.edu>
2024-12-12 00:44:08 -0500

    h2 database set up and user entity + userinfo entity created

--- socialmedia/src/main/java/dev/michaelcao512/socialmedia/Controllers/MainController.java
+++ socialmedia/src/main/java/dev/michaelcao512/socialmedia/Controllers/MainController.java
@@ -1,8 +1,13 @@
 package dev.michaelcao512.socialmedia.Controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 public class MainController {
-    
+    @GetMapping("test")
+    public String helloWorld() {
+        return "Hello World";
+    }
 }
---
+++ socialmedia/src/main/java/dev/michaelcao512/socialmedia/Entities/Account.java
@@ -0,0 +1,39 @@
+package dev.michaelcao512.socialmedia.Entities;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PreUpdate;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long accountId;
+
+    @Column(unique = true)
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @Column(updatable = false)
+    private LocalDateTime dateCreated = LocalDateTime.now();
+    private LocalDateTime dateUpdated = LocalDateTime.now();
+
+    @OneToOne(mappedBy = "account", cascade = CascadeType.ALL)
+    private UserInfo userInfo;
+
+    @PreUpdate
+    public void onUpdate() {
+        this.dateUpdated = LocalDateTime.now();
+    }
+}
